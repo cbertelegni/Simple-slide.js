72f7bd6d320618c6c933a8ab9f861c64ed10eb6e
Cristian Bertelegni <cristianbertelegni@gmail.com>
2014-03-17 09:52:54 -0300

    surce

---
+++ simpleslide.js
@@ -0,0 +1,51 @@
+/*
+*
+* @cbertelegni
+* Github: https://github.com/cbertelegni/Simple-slide.js
+* requiere jquery
+* optional jquery.easing
+*
+*/
+
+function simple_slide(app){
+		app.lis=$("li", app.ul);
+		app.w= $(app.lis[0]).width();
+		app.wContent= app.lis.length * app.w;
+		// console.log($(lis[0]).width());
+		app.ul.css({ // set width para el ul
+			"width": app.wContent
+		});
+		app.next.click(function(){
+			 moverUl(1);
+			 return false;
+		});
+		app.prev.click(function(){
+			 moverUl(0);
+			 return false;
+		});
+		validaMarginUl();
+	function moverUl(btn){
+		if (btn) { //next
+			var marginLeft="-="+app.w;
+		}else{ // prev
+			var marginLeft="+="+app.w;
+		};
+		app.ul.stop(0,1).animate({"margin-left": marginLeft},  500, app.efecto_animacion, function(){validaMarginUl()});
+	}
+	function validaMarginUl(){
+		var marginUl = parseInt(app.ul.css("margin-left"));
+		
+		if(0 <= marginUl){
+			app.ul.stop(0, 1).animate({"margin-left": 0}, 100);
+			app.prev.hide();
+		}else{
+			app.prev.show();
+		}
+		if((marginUl + app.wContent) <= app.w ){
+			app.ul.stop(0, 1).animate({"margin-left": (app.w -app.wContent)}, 100);
+			app.next.hide();
+		}else{
+			app.next.show();
+		}
+	}
+}
